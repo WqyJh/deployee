d07c2d68325137f45cd9815413a3543910ce815d
WqyJh <781345688@qq.com>
2019-08-16 17:29:31 +0800

    feat: add attr evaluation to format

--- format.py
+++ format.py
@@ -7,6 +7,19 @@ def format(template: str) -> str:
     d = calframe.f_globals.copy()
     d.update(calframe.f_locals)
 
-    keys = re.findall(r'{(\w+?)}', template)
-    args = {k: d.get(k, '') for k in keys}
+    keys = re.findall(r'{([\w.]+?)}', template)
+
+    def _eval(k, d):
+        if '.' in k:
+            ret = ''
+            key, path = k.split('.', maxsplit=1)
+            obj = d.get(key, None) or ''
+            # if obj:
+            #     ret = operator.attrgetter(path)(obj)
+            #     ret = ret if ret else ''
+            return key, obj
+        return k, d.get(k, '')
+
+    args = dict(_eval(k, d) for k in keys)
+    print(args, template)
     return template.format(**args)
--- test.py
+++ test.py
@@ -6,19 +6,21 @@ class TestFormat(unittest.TestCase):
     def test_format(self):
         k1 = 'val1'
         k2 = 'val2'
-        # setattr(k1, 'subkey', 'subval')
+
+        class A:
+            a = 'class'
+        k4 = A()
 
         asserts = (
             ('val1 & val2', _('{k1} & {k2}')),
             ('val1val2', _('{k1}{k2}')),
             ('val1', _('{k1}{k3}')),
-            # ('subval+val2', _('{k1.subkey}+{k2}')),
+            ('class+val2', _('{k4.a}+{k2}')),
         )
-        
+
         for expected, value in asserts:
             print(expected, value)
             self.assertEqual(expected, value)
-        
 
 
 if __name__ == '__main__':
