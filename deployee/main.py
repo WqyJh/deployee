bd85aacc93b98aa8256d0eba10e0bee0c4356547
WqyJh <781345688@qq.com>
2019-12-09 22:39:27 +0800

    feat: generate vars.yml

--- deployee/main.py
+++ deployee/main.py
@@ -1,10 +1,45 @@
 import os
+import uuid
+import yaml
+import shutil
 import subprocess
 
 
+def git_rev(repo):
+    ret = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=repo)
+    ret = ret.decode().strip()
+    return ret
+
+
 def main():
-    os.chdir('./ansible')
+    owd = os.getcwd()
+    wd = f'tmp-{uuid.uuid4()}'
+
+    shutil.copytree('ansible', wd)
+    os.chdir(wd)
+
+    vars = {
+        'project_name': 'dian_recruit',
+        'stage': 'staging',
+        'project_src': '/home/linux/Works/VSCodeProject/dian_recruit/Dian',
+        'supervisord_conf': '/etc/supervisord.conf',
+        'supervisor_prefix': '/etc/supervisord.d',
+        'supervisor_ext': 'ini',
+        'gunicorn_conf': 'gunicorn.conf.py',
+        'wsgi_app': 'Dian.wsgi:application',
+        'postfix': 'f606bc8',
+    }
+
+    vars['postfix'] = git_rev(vars['project_src'])
+
+    with open('vars.yml', 'w') as f:
+        f.write(yaml.dump(vars))
+
     subprocess.call(['ansible-playbook', 'playbook.yml'])
+
+
+    os.chdir(owd)
+    shutil.rmtree(wd)
 
 
 if __name__ == '__main__':
