312342e479e766e377856089b9ebf98294131ed8
WqyJh <781345688@qq.com>
2019-12-17 17:27:21 +0800

    feat: add click to create cli

--- deployee/main.py
+++ deployee/main.py
@@ -1,42 +1,57 @@
 import os
 import uuid
 import yaml
+import click
 import shutil
 import subprocess
 
+from . import __version__
+
 
 def git_rev(repo):
-    ret = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=repo)
-    ret = ret.decode().strip()
-    return ret
+    try:
+        ret = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=repo)
+        ret = ret.decode().strip()
+        return ret
+    except Exception as e:
+        print(f'{repo} is not a git repo')
 
 
-def main():
+
+@click.command()
+@click.argument('project')
+@click.argument('stage', type=click.Choice(['test', 'staging', 'production']))
+@click.option('-n', '--name')
+@click.option('--gunicorn-conf', default='gunicorn.conf.py')
+@click.option('--wsgi-app')
+@click.option('--postfix')
+def main(project, stage, name=None, gunicorn_conf=None, wsgi_app=None, postfix=None):
+    project = os.path.abspath(project)
+    name = name or os.path.basename(project)
+    postfix = postfix or git_rev(project) or ''
+    wsgi_app = wsgi_app or f'{name}.wsgi:application'
+
     owd = os.getcwd()
-    wd = f'tmp-{uuid.uuid4()}'
+    wd = f'deployee-{uuid.uuid4()}'
 
     shutil.copytree('ansible', wd)
     os.chdir(wd)
 
     vars = {
-        'project_name': 'dian_recruit',
-        'stage': 'staging',
-        'project_src': '/home/linux/Works/VSCodeProject/dian_recruit/Dian',
-        'supervisord_conf': '/etc/supervisord.conf',
-        'supervisor_prefix': '/etc/supervisord.d',
-        'supervisor_ext': 'ini',
-        'gunicorn_conf': 'gunicorn.conf.py',
-        'wsgi_app': 'Dian.wsgi:application',
-        'postfix': 'f606bc8',
+        'project_name': name,
+        'stage': stage,
+        'project_src': project,
+        'gunicorn_conf': gunicorn_conf,
+        'wsgi_app': wsgi_app,
+        'postfix': postfix,
     }
 
-    vars['postfix'] = git_rev(vars['project_src'])
+    print(vars)
 
     with open('vars.yml', 'w') as f:
         f.write(yaml.dump(vars))
 
     subprocess.call(['ansible-playbook', 'playbook.yml'])
-
 
     os.chdir(owd)
     shutil.rmtree(wd)
